3cad51c84d008794e944a2307c287d10a6f260f6
ademzeri <87881799+ademzeri@users.noreply.github.com>
2022-12-26 19:04:18 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'mybg':"url('/assests/frame 5.png')"
+      }
+    },
+  },
+  plugins: [],
+}
